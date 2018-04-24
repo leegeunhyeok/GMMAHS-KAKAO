ef920e1e1fa68a6616c81d0efb76f01838b66746
Leegeunhyeok <lghlove0509@naver.com>
2018-04-24 20:16:38 +0900

    Change js to ts

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_js_1 = require("./app/app.js");
-const app = new app_js_1.default();
-app.init(8080).then(() => {
-    app.start();
-});
