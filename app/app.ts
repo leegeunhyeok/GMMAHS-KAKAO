64f51479430817e9f582e7a186e098af2924693f
Leegeunhyeok <lghlove0509@naver.com>
2018-04-24 20:45:33 +0900

    App start() method fix

--- app/app.ts
+++ app/app.ts
@@ -116,7 +116,7 @@ class App {
 
     // 관리자 전용 페이지 
     router.route(admin.getRoute()).get((req: express.Request, res: express.Response): void => {
-      fs.readFile('./static/test.html', (err, data) => {
+      fs.readFile('./static/admin.html', (err, data) => {
         if(err) {
           console.log(err);
           res.writeHead(200, {'Content-Type': 'text/html'});
@@ -370,26 +370,31 @@ class App {
     http.createServer(this.app).listen(this.port, async () => {
       this.logger('Gmmahs KAKAO server started');
       try {
+        
         // 서버 실행 후 데이터 세팅 
-        await this.meal.set(false);
-        await this.calendar.set();
-        await this.weather.set();
         try {
-          await this.timetable.set();
+          this.logger(await this.meal.set(false));
+          //this.logger(await this.calendar.set());
+          this.logger(await this.weather.set());
+          this.logger(await this.timetable.set());
         } catch(e) {
           this.logger(e);
         }
 
         // 매일 00:00:01 급식데이터 및 이번달 일정 데이터 갱신
         schedule.scheduleJob('1 0 0 * * * *', async (): Promise<any> => {
-          await this.meal.set(false);
-          await this.calendar.set();
+          this.logger(await this.meal.set(false));
+          this.logger(await this.calendar.set());
         });
 
         // 매주 토요일 00:00:00에 시간표 데이터 갱신
         schedule.scheduleJob('0 0 0 * * * 7', (): void => {
           try {
-            this.timetable.set();
+            this.timetable.set().then((result) => {
+          
+            }).catch((e) => {
+            
+            });
           } catch(e) {
             this.logger(e);
           }
@@ -398,12 +403,20 @@ class App {
         // 매일 14:00:00 급식데이터 갱신 (내일 급식으로)
         // 점심시간이 지난 후 (2시에 내일 급식으로 갱신)
         schedule.scheduleJob('0 0 14 * * * *', (): void => {
-          this.meal.set(true);
+          this.meal.set(true).then((result) => {
+          
+          }).catch((e) => {
+          
+          });
         });
 
         // 매 시간마다 날씨데이터 갱신
         schedule.scheduleJob('0 0 * * * * *', (): void => {
-          this.weather.set();  
+          this.weather.set().then((result) => {
+          
+          }).catch((e) => {
+          
+          }); 
         });
       } catch(e) {
         console.log(e);
